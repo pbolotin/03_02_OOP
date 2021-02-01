f85a62c6b30d310a204b87d2dae92aa2279b6952
pbolotin <Pavel.Bolotin@gmail.com>
2021-02-01 22:34:32 +0700

    Change snake.cpp to test Ticker class

--- snake.cpp
+++ snake.cpp
@@ -1,6 +1,19 @@
+#include <iostream>
 #include "GameField.h"
+#include "Ticker.h"
+
+using namespace std;
 
 int main() {
-    GameField gf;
+    Ticker* ticker = new Ticker(1000000);
+    int ret_val;
+    while(1) {
+        ret_val = ticker->do_tick();
+        if(0 == ret_val) {
+            cout << "tick ok" << endl;
+        } else {
+            cout << "tick wrong" << endl;
+        }
+    }
     return 0;
 }
