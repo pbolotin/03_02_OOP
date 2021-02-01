97b81f4e5d6eb7ba212d8ff4cd5d19fc1422f198
pbolotin <Pavel.Bolotin@gmail.com>
2021-02-01 17:30:34 +0700

    Add GameField.cpp GameField.h with simple class and its realisation

---
+++ GameField.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "GameField.h"
+
+using namespace std;
+
+GameField::GameField(): i(0) {
+    cout << "GameField::GameField()" << endl;
+}
---
+++ GameField.h
@@ -0,0 +1,10 @@
+#ifndef _GameField_
+#define _GameField_
+
+class GameField {
+    int i;
+public:
+    GameField();
+};
+
+#endif
