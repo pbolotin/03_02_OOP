1a1473d0adaed1f85ab22a7a252ea40dbf464296
pbolotin <Pavel.Bolotin@gmail.com>
2021-02-07 19:23:41 +0700

    Add member of the User class into the GameProccess class

--- GameProcess.h
+++ GameProcess.h
@@ -1,9 +1,11 @@
 #ifndef _GameProcess_
 #define _GameProcess_
 #include "GameField.h"
+#include "User.h"
 
 class GameProcess {
     GameField gf;
+    User user;
 public:
     GameProcess();
     ~GameProcess();
