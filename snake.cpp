79a9d95184c5c11031cfb3999e2ea0b34e4339c2
pbolotin <Pavel.Bolotin@gmail.com>
2021-02-07 13:39:02 +0700

    Change snake.cpp to test the ConsoleSwitcher class

--- snake.cpp
+++ snake.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include "GameField.h"
 #include "Ticker.h"
+#include "ConsoleSwitcher.h"
 
 using namespace std;
 
 int main() {
     Ticker* ticker = new Ticker(1000000);
     int ret_val;
-    while(1) {
+    int i = 0;
+    ConsoleSwitcher::storeCurrentTerminalState();
+    ConsoleSwitcher::setNotCanonicalTerminalState();
+    while(i < 10) {
         ret_val = ticker->do_tick();
         if(0 == ret_val) {
             cout << "tick ok" << endl;
         } else {
             cout << "tick wrong" << endl;
         }
+        i++;
     }
+    ConsoleSwitcher::restoreTerminalState();
     return 0;
 }
