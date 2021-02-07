78c2ab2f36efb0b4206bafd2b0421cd0fd11a43a
pbolotin <Pavel.Bolotin@gmail.com>
2021-02-07 13:18:54 +0700

    Remove from Ticker.h unused methods

--- Ticker.h
+++ Ticker.h
@@ -10,10 +10,6 @@ public:
     Ticker(unsigned long tick_length);
     Ticker(unsigned long tick_length, unsigned long tick_counter);
     int do_tick();
-    unsigned long set_tick_length(unsigned long);
-    unsigned long get_tick_length();
-    unsigned long set_tick_count(unsigned long);
-    unsigned long get_tick_count();
 };
 
 #endif
