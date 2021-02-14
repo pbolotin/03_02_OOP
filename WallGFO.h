77566415c57c85624919805975d9c90ba17f00d0
pbolotin <Pavel.Bolotin@gmail.com>
2021-02-14 21:38:52 +0700

    Partial commit. WallGFO class new methods and variables.
    1. WallGFO class now has WallGFO() constructor, to initialize
    wayToPlace and wallUCharSymbol variables.
    
    2. new toPlaceOnGameField(GameField &) method, it place on GameField
    wall values (along GameField border).

--- WallGFO.h
+++ WallGFO.h
@@ -4,11 +4,16 @@
 #include "GameFieldObject.h"
 #include "GameField.h"
 
+#define WALL_AROUND_GAME_FIELD 1
+#define WALL_MATRIX_VALUE 1
+
 class WallGFO : GameFieldObject {
 private:
-
+    unsigned wayToPlace;
+    unsigned char wallUCharSymbol;
 public:
-    int placeOnGameField(GameField& gf);
+    WallGFO();
+    int toPlaceOnGameField(GameField& gf);
     ~WallGFO() = default;
 };
 
