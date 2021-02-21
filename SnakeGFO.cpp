d8c39c3c646f37eebc1c75d5fc5180cc1c0282fe
pbolotin <Pavel.Bolotin@gmail.com>
2021-02-21 13:15:19 +0700

    SnakeGFO class, add the ActiveObject methods realization

--- SnakeGFO.cpp
+++ SnakeGFO.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-SnakeGFO::SnakeGFO(): snakeGFCode(SNAKE_GF_CODE) {}
+SnakeGFO::SnakeGFO(): snakeGFCode(SNAKE_GF_CODE), moveDirection(SNAKE_MOVE_UP) {}
 
 int SnakeGFO::setOnGameField(GameField& gf) {
     unsigned sizeXY[2];
@@ -16,4 +16,36 @@ int SnakeGFO::setOnGameField(GameField& gf) {
     gf.setValueByCoordsXY((unsigned)this->snakeGFCode, coordsXY);
     
     return 0;
+}
+
+int SnakeGFO::reactOnGameField(GameField& gf) {
+    unsigned coordsXY[2];
+    coordsXY[0] = 0;
+    coordsXY[1] = 0;
+    unsigned value;
+    gf.getValueByCoordsXY(&value, coordsXY);
+    return 0;
+}
+
+int SnakeGFO::reactOnUser(User& ur) {
+    switch(ur.getLastWill()) {
+        case USER_WANT_SNAKE_UP:
+            this->moveDirection = SNAKE_MOVE_UP;
+            break;
+        case USER_WANT_SNAKE_DOWN:
+            this->moveDirection = SNAKE_MOVE_DOWN;
+            break;
+        case USER_WANT_SNAKE_RIGHT:
+            this->moveDirection = SNAKE_MOVE_RIGHT;
+            break;
+        case USER_WANT_SNAKE_LEFT:
+            this->moveDirection = SNAKE_MOVE_LEFT;
+            break;
+    }
+    return 0;
+}
+
+int SnakeGFO::reactOnTicker(Ticker& tr) {
+    unsigned long tc = tr.getTickCount();
+    return tc;
 }
--- SnakeGFO.h
+++ SnakeGFO.h
@@ -2,15 +2,26 @@
 #define _SnakeGFO_h_
 
 #include "GameFieldObject.h"
+#include "ActiveObject.h"
 #include "GameField.h"
 
-#define SNAKE_GF_CODE 2
 
-class SnakeGFO : GameFieldObject {
+#define SNAKE_GF_CODE       2
+
+#define SNAKE_MOVE_UP       1
+#define SNAKE_MOVE_DOWN     2
+#define SNAKE_MOVE_LEFT     3
+#define SNAKE_MOVE_RIGHT    4
+
+class SnakeGFO : GameFieldObject, ActiveObject {
 private:
     unsigned char snakeGFCode;
+    unsigned moveDirection;
 public:
     SnakeGFO();
+    int reactOnGameField(GameField& gf);
+    int reactOnUser(User& ur);
+    int reactOnTicker(Ticker& tr);
     int setOnGameField(GameField& gf);
     ~SnakeGFO() = default;
 };
