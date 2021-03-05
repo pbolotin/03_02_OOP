b3f1dcec70711d813e2fd3e06f6ff4cc705c757a
pbolotin <Pavel.Bolotin@gmail.com>
2021-03-05 16:11:07 +0700

    [Partial] Many changes in the SnakeGFO class. New members and methods.
    It is needed to get base moving of the snake.

--- SnakeGFO.cpp
+++ SnakeGFO.cpp
@@ -4,48 +4,123 @@
 
 using namespace std;
 
-SnakeGFO::SnakeGFO(): snakeGFCode(SNAKE_GF_CODE), moveDirection(SNAKE_MOVE_UP) {}
+SnakeGFO::SnakeGFO(): snakeGFCode(SNAKE_GF_CODE),
+                      moveDirection(SNAKE_MOVE_UP),
+                      canMove(SNAKE_CAN_MOVE),
+                      tickReductorQuotient(SNAKE_TICK_REDUCTOR),
+                      finishFlag(SNAKE_NOT_FINISH)
+{}
 
-int SnakeGFO::setOnGameField(GameField& gf) {
-    unsigned sizeXY[2];
-    gf.getSizeXY(sizeXY);
-    
-    unsigned coordsXY[2];
-    coordsXY[0] = sizeXY[0]/2;
-    coordsXY[1] = sizeXY[1]/2;
-    gf.setValueByCoordsXY((unsigned)this->snakeGFCode, coordsXY);
-    
+int SnakeGFO::setHeadCoordsXY(unsigned headCoordsXY[2]) {
+    this->headCoordsXY[0] = headCoordsXY[0];
+    this->headCoordsXY[1] = headCoordsXY[1];
     return 0;
 }
 
-int SnakeGFO::reactOnGameField(GameField& gf) {
-    unsigned coordsXY[2];
-    coordsXY[0] = 0;
-    coordsXY[1] = 0;
-    unsigned value;
-    gf.getValueByCoordsXY(&value, coordsXY);
+int SnakeGFO::setOnGameField(GameField& gf) {
+    gf.setValueByCoordsXY((unsigned)this->snakeGFCode, this->headCoordsXY);
     return 0;
 }
 
 int SnakeGFO::reactOnUser(User& ur) {
     switch(ur.getLastWill()) {
         case USER_WANT_SNAKE_UP:
-            this->moveDirection = SNAKE_MOVE_UP;
+            if(this->moveDirection != SNAKE_MOVE_DOWN) {
+                this->moveDirection = SNAKE_MOVE_UP;
+            }
             break;
         case USER_WANT_SNAKE_DOWN:
-            this->moveDirection = SNAKE_MOVE_DOWN;
+            if(this->moveDirection != SNAKE_MOVE_UP) {
+                this->moveDirection = SNAKE_MOVE_DOWN;
+            }
             break;
         case USER_WANT_SNAKE_RIGHT:
-            this->moveDirection = SNAKE_MOVE_RIGHT;
+            if(this->moveDirection != SNAKE_MOVE_LEFT) {
+                this->moveDirection = SNAKE_MOVE_RIGHT;
+            }
             break;
         case USER_WANT_SNAKE_LEFT:
-            this->moveDirection = SNAKE_MOVE_LEFT;
+            if(this->moveDirection != SNAKE_MOVE_RIGHT) {
+                this->moveDirection = SNAKE_MOVE_LEFT;
+            }
             break;
     }
     return 0;
 }
 
+int SnakeGFO::reactOnGameField(GameField& gf) {
+    unsigned XY[2];
+    XY[0] = this->headCoordsXY[0];
+    XY[1] = this->headCoordsXY[1];
+    
+    switch(this->moveDirection) {
+        case SNAKE_MOVE_UP:
+            XY[1] -= 1;
+            break;
+        case SNAKE_MOVE_DOWN:
+            XY[1] += 1;
+            break;
+        case SNAKE_MOVE_RIGHT:
+            XY[0] += 1;
+            break;
+        case SNAKE_MOVE_LEFT:
+            XY[0] -= 1;
+            break;
+    }
+    
+    unsigned value;
+    gf.getValueByCoordsXY(&value, XY);
+    
+    switch(value) {
+        case EMPTY_GF_CODE:
+            this->canMove = SNAKE_CAN_MOVE;
+            break;
+        case SNAKE_GF_CODE:
+            this->canMove = SNAKE_CANT_MOVE;
+            break;
+        case WALL_GF_CODE:
+            this->canMove = SNAKE_CANT_MOVE;
+            break;
+        default:
+            this->canMove = SNAKE_CANT_MOVE;
+            break;
+    }
+    
+    return 0;
+}
+
 int SnakeGFO::reactOnTicker(Ticker& tr) {
     unsigned long tc = tr.getTickCount();
-    return tc;
+    if(0 == tc % this->tickReductorQuotient) {
+        if(SNAKE_CANT_MOVE == this->canMove) {
+            this->finishFlag = SNAKE_FINISH;
+        } else {
+            this->move();
+        }
+    }
+    return 0;
+}
+
+int SnakeGFO::move() {
+    switch(this->moveDirection) {
+        case SNAKE_MOVE_UP:
+            this->headCoordsXY[1] -= 1;
+            break;
+        case SNAKE_MOVE_DOWN:
+            this->headCoordsXY[1] += 1;
+            break;
+        case SNAKE_MOVE_RIGHT:
+            this->headCoordsXY[0] += 1;
+            break;
+        case SNAKE_MOVE_LEFT:
+            this->headCoordsXY[0] -= 1;
+            break;
+    }
+    return 0;
+}
+
+int SnakeGFO::check_if_finish() {
+    if(SNAKE_FINISH == this->finishFlag) {
+        return 1;
+    } else return 0;
 }
--- SnakeGFO.h
+++ SnakeGFO.h
@@ -5,6 +5,7 @@
 #include "ActiveObject.h"
 #include "GameField.h"
 
+#define SNAKE_TICK_REDUCTOR 10
 
 #define SNAKE_GF_CODE       2
 
@@ -13,16 +14,32 @@
 #define SNAKE_MOVE_LEFT     3
 #define SNAKE_MOVE_RIGHT    4
 
+#define SNAKE_CAN_MOVE      1
+#define SNAKE_CANT_MOVE     0
+
+#define SNAKE_FINISH        1
+#define SNAKE_NOT_FINISH    0
+
 class SnakeGFO : GameFieldObject, ActiveObject {
 private:
     unsigned char snakeGFCode;
     unsigned moveDirection;
+    unsigned canMove;
+    unsigned tickReductorQuotient;
+    unsigned finishFlag;
+    
+    unsigned headCoordsXY[2];
 public:
     SnakeGFO();
-    int reactOnGameField(GameField& gf);
     int reactOnUser(User& ur);
+    int reactOnGameField(GameField& gf);
     int reactOnTicker(Ticker& tr);
+    
+    int setHeadCoordsXY(unsigned headCoordsXY[2]);
     int setOnGameField(GameField& gf);
+    
+    int move();
+    int check_if_finish();
     ~SnakeGFO() = default;
 };
 
