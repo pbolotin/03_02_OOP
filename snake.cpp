ccca03b1ac736b3fcdc672e66216e94fd677e5dc
pbolotin <Pavel.Bolotin@gmail.com>
2021-03-05 16:17:22 +0700

    To simplificate main. Remove Ticker and unnecessary things. All the rest about moving of the snake.

--- snake.cpp
+++ snake.cpp
@@ -7,25 +7,16 @@
 using namespace std;
 
 int main() {
-    Ticker* ticker = new Ticker(100000);
-    int ret_val;
-    int i = 0;
     ConsoleSwitcher::storeCurrentTerminalState();
     ConsoleSwitcher::setNotCanonicalTerminalState();
     GameProcess gp;
     GameRepresentator gr;
-    while(!gp.it_is_the_end()) {
-        ret_val = ticker->do_tick();
-        if(0 == ret_val) {
-            cout << "tick ok" << endl;
-        } else {
-            cout << "tick wrong" << endl;
-        }
-        i++;
+    do {
+        gp.doGamePause();
         gp.setUserWillByKey(ConsoleSwitcher::getLastKeyFromTerminal());
         gp.doGameProcessStep();
         gr.outputGameFieldOnTerminal(gp.getGameField());
-    }
+    } while(!gp.it_is_the_end());
     ConsoleSwitcher::restoreTerminalState();
     return 0;
 }
